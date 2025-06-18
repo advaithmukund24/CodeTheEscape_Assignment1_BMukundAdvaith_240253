e48eab97bcfae378e38fcc0765194f273a57f3a4
advaithmukund24 <advaithmukund24@gmail.com>
2025-06-18 23:30:44 +0530

    Update ReverseArray.cpp

--- ReverseArray.cpp
+++ ReverseArray.cpp
@@ -1,12 +1,15 @@
-void reverseArray (vector<int> &arr, int m){
-  int len = arr.size();
-  int a = m+1;
-  int b = len-1;
-  while (a<b){
-    int p = arr[a]; 
-    arr[a] = arr[b]; 
-    arr[b] = p;
-    a++;
-    b--;
-  }
+void reverseArray(vector<int> &arr , int m) {
+    int len = arr.size();
+    
+    int a = m+1;
+    int b = len-1;
+
+    while (a<b){
+        int p = arr[a];
+        arr[a] = arr[b];
+        arr[b] = p;
+
+        a++;
+        b--;
+    }
 }
