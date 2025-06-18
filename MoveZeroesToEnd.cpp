c1a5a2ebadb223a5aa76188f0e9f5784393b640d
advaithmukund24 <advaithmukund24@gmail.com>
2025-06-18 23:32:08 +0530

    Update MoveZeroesToEnd.cpp

--- MoveZeroesToEnd.cpp
+++ MoveZeroesToEnd.cpp
@@ -1,18 +1,18 @@
-void pushZerosAtEnd(vector<int> &arr)
+void pushZerosAtEnd(vector<int> &arr) 
 {
-  int len = arr.size();
-  int x=0;
-  for (int i=0; i<len; i++){
-    if (arr[i]==0){
-      continue;
-    }
-    else{
-      arr[x] = arr[i];
-      x++;
-    }
-  }
-  while (x< len) {
-    arr[x] = 0;
-    x++;
-  }
+	int len = arr.size();
+	int x=0;
+	for (int i=0; i<len; i++){
+		if (arr[i]==0){
+			continue;
+		}
+		else{
+			arr[x]=arr[i];
+			x++;
+		}
+	}
+	while (x< len){
+		arr[x] = 0;
+		x++;
+	}
 }
